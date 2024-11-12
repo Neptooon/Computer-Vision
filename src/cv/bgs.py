6bcb683836db760f361ebb83620114dbf357601e
David Stevic <stevic.d@outlook.de>
2024-11-12 14:04:00 +0100

    BGS Parameteranpassung

--- src/cv/bgs.py
+++ src/cv/bgs.py
@@ -27,11 +27,12 @@ def bgs():
     cap = cv.VideoCapture('../../assets/videos/Cross-Default-Pulli-Hell.mov')
 
     # BG-methoden
-    MOG2 = cv.createBackgroundSubtractorMOG2(detectShadows=True)
+    MOG2 = cv.createBackgroundSubtractorMOG2(detectShadows=True, varThreshold=32)
     CNT = cv.bgsegm.createBackgroundSubtractorCNT()
-    KNN = cv.createBackgroundSubtractorKNN(detectShadows=True)
+    KNN = cv.createBackgroundSubtractorKNN(detectShadows=True, dist2Threshold=1000)
 
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (8, 8))
+    kernel2 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (10, 10))
 
     while True:
         ret, frame = cap.read()
@@ -45,12 +46,11 @@ def bgs():
         fgmask2 = CNT.apply(frame)
         fgmask3 = KNN.apply(frame)
 
-
         # Schatten entfernen
         fgmask = cv.threshold(fgmask, 254, 255, cv.THRESH_BINARY)[1]
 
         fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)  # Rauschen entfernen
-        fgmask = cv.morphologyEx(fgmask, cv.MORPH_CLOSE, kernel)  # Löcher füllen
+        fgmask = cv.morphologyEx(fgmask, cv.MORPH_CLOSE, kernel2)  # Löcher füllen
 
         # Median-Filter zur weiteren Rauschreduzierung
         fgmask = cv.medianBlur(fgmask, 5)
@@ -58,7 +58,7 @@ def bgs():
         # Schatten entfernen
         fgmask2 = cv.threshold(fgmask2, 254, 255, cv.THRESH_BINARY)[1]
         fgmask2 = cv.morphologyEx(fgmask2, cv.MORPH_OPEN, kernel)  # Rauschen entfernen
-        fgmask2 = cv.morphologyEx(fgmask2, cv.MORPH_CLOSE, kernel)  # Löcher füllen
+        fgmask2 = cv.morphologyEx(fgmask2, cv.MORPH_CLOSE, kernel2)  # Löcher füllen
 
         # Median-Filter zur weiteren Rauschreduzierung
         fgmask2 = cv.medianBlur(fgmask2, 5)
@@ -66,12 +66,12 @@ def bgs():
         # Schatten entfernen
         fgmask3 = cv.threshold(fgmask3, 254, 255, cv.THRESH_BINARY)[1]
         fgmask3 = cv.morphologyEx(fgmask3, cv.MORPH_OPEN, kernel)  # Rauschen entfernen
-        fgmask3 = cv.morphologyEx(fgmask3, cv.MORPH_CLOSE, kernel)  # Löcher füllen
+        fgmask3 = cv.morphologyEx(fgmask3, cv.MORPH_CLOSE, kernel2)  # Löcher füllen
 
         # Median-Filter zur weiteren Rauschreduzierung
         fgmask3 = cv.medianBlur(fgmask3, 5)
 
-        #fgmask2 = cv.bitwise_and(fgmask2, fgmask)
+
 
         # Masken in BGR umwandeln, weil sonst die Darstellung von Farb- und Graubild von den Channeln her nicht passt
         fgmask_bgr = cv.cvtColor(fgmask, cv.COLOR_GRAY2BGR)
