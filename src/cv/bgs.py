18c24f0507acf16ddd957ba3263ab05626e39325
Lukas Landmann <lukaslandmann69@gmail.com>
2024-11-14 09:31:10 +0100

    BGS fertig und dokumentiert

--- src/cv/bgs.py
+++ src/cv/bgs.py
@@ -9,71 +9,65 @@ def bgs():
         font = cv.FONT_HERSHEY_SIMPLEX
         font_scale = 0.8
         color = (255, 0, 0)
-        thickness = 2
+        size = 2
 
         frame_width = frame.shape[1]
-        box_x = frame_width - 60
-        box_y = 30
+        x = frame_width - 60
+        y = 30
 
-        cv.putText(frame, name, (10, 30), font, font_scale, color, thickness, cv.LINE_AA)
-        cv.rectangle(frame, (box_x - 5, box_y - 15), (box_x + 50, box_y + 5), (255, 255, 255), -1)
-        cv.putText(frame, str(int(cap.get(cv.CAP_PROP_POS_FRAMES))), (box_x, box_y), font, 0.5, (0, 0, 0))
+        cv.putText(frame, name, (10, 30), font, font_scale, color, size, cv.LINE_AA)
+        cv.rectangle(frame, (x - 5, y - 15), (x + 50, y + 5), (255, 255, 255), -1)
+        cv.putText(frame, str(int(cap.get(cv.CAP_PROP_POS_FRAMES))), (x, y), font, 0.5, (0, 0, 0)) #frames oben rechts
 
 
-    # Zielbreite für das kombinierte Bild
+    # Zielbreite für das Entbild
     target_width = 1180
 
     # Video laden
-    cap = cv.VideoCapture('../../assets/videos/Cross-Default-Pulli-Hell.mov')
+    cap = cv.VideoCapture('../../assets/videos/LL-Default-Pulli-Hell-LR.mov')
 
-    # BG-methoden
+    # BGS-methoden mit Parametrisierung
     MOG2 = cv.createBackgroundSubtractorMOG2(detectShadows=True, varThreshold=32)
+
     CNT = cv.bgsegm.createBackgroundSubtractorCNT()
     KNN = cv.createBackgroundSubtractorKNN(detectShadows=True, dist2Threshold=1000)
 
-    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (8, 8))
-    kernel2 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (10, 10))
+    # Kernel für Morph. Operatoren
+    kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
 
     while True:
         ret, frame = cap.read()
         if frame is None:
             break
 
-        frame = cv.GaussianBlur(frame, (5, 5), 0)
+        frame = cv.GaussianBlur(frame, (5, 5), 0)  # Rauschunterdrückung
 
         # BGS anwenden
         fgmask = MOG2.apply(frame)
         fgmask2 = CNT.apply(frame)
         fgmask3 = KNN.apply(frame)
 
-        # Schatten entfernen
+        # Schatten von den Vordergrundmasken über Threshold entfernen
         fgmask = cv.threshold(fgmask, 254, 255, cv.THRESH_BINARY)[1]
-
         fgmask = cv.morphologyEx(fgmask, cv.MORPH_OPEN, kernel)  # Rauschen entfernen
-        fgmask = cv.morphologyEx(fgmask, cv.MORPH_CLOSE, kernel2)  # Löcher füllen
+        fgmask = cv.morphologyEx(fgmask, cv.MORPH_CLOSE, kernel)  # Löcher füllen
 
         # Median-Filter zur weiteren Rauschreduzierung
-        fgmask = cv.medianBlur(fgmask, 5)
+        fgmask = cv.medianBlur(fgmask, 3)
 
-        # Schatten entfernen
+        # ... Gleicher Code = Gleiche Doku s.o.
         fgmask2 = cv.threshold(fgmask2, 254, 255, cv.THRESH_BINARY)[1]
-        fgmask2 = cv.morphologyEx(fgmask2, cv.MORPH_OPEN, kernel)  # Rauschen entfernen
-        fgmask2 = cv.morphologyEx(fgmask2, cv.MORPH_CLOSE, kernel2)  # Löcher füllen
+        fgmask2 = cv.morphologyEx(fgmask2, cv.MORPH_OPEN, kernel)
+        fgmask2 = cv.morphologyEx(fgmask2, cv.MORPH_CLOSE, kernel)
+        fgmask2 = cv.medianBlur(fgmask2, 3)
 
-        # Median-Filter zur weiteren Rauschreduzierung
-        fgmask2 = cv.medianBlur(fgmask2, 5)
-
-        # Schatten entfernen
+        # ...
         fgmask3 = cv.threshold(fgmask3, 254, 255, cv.THRESH_BINARY)[1]
-        fgmask3 = cv.morphologyEx(fgmask3, cv.MORPH_OPEN, kernel)  # Rauschen entfernen
-        fgmask3 = cv.morphologyEx(fgmask3, cv.MORPH_CLOSE, kernel2)  # Löcher füllen
+        fgmask3 = cv.morphologyEx(fgmask3, cv.MORPH_OPEN, kernel)
+        fgmask3 = cv.morphologyEx(fgmask3, cv.MORPH_CLOSE, kernel)
+        fgmask3 = cv.medianBlur(fgmask3, 3)
 
-        # Median-Filter zur weiteren Rauschreduzierung
-        fgmask3 = cv.medianBlur(fgmask3, 5)
-
-
-
-        # Masken in BGR umwandeln, weil sonst die Darstellung von Farb- und Graubild von den Channeln her nicht passt
+        # Masken in BGR-Bild umwandeln, weil sonst die Darstellung von Farb- und Graubild von den Channeln her nicht passt
         fgmask_bgr = cv.cvtColor(fgmask, cv.COLOR_GRAY2BGR)
         fgmask2_bgr = cv.cvtColor(fgmask2, cv.COLOR_GRAY2BGR)
         fgmask3_bgr = cv.cvtColor(fgmask3, cv.COLOR_GRAY2BGR)
@@ -91,7 +85,7 @@ def bgs():
         bottom_row = np.hstack((fgmask3_bgr, fgmask2_bgr))
         combined_frame = np.vstack((top_row, bottom_row))
 
-        # Bild skalieren auf Zielbreite
+        # Bild skalieren auf die oben angegebene Größe
         height, width = combined_frame.shape[:2]
         scaling_factor = target_width / width
         resized_frame = cv.resize(combined_frame, (int(width * scaling_factor), int(height * scaling_factor)))
