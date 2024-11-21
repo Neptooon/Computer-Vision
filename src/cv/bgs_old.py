307957ca477dd55352fb611a7ca35e9ad8965d31
David Stevic <stevic.d@outlook.de>
2024-11-21 17:21:31 +0100

    BGS nur mit MOG2

--- src/cv/bgs_old.py
+++ src/cv/bgs_old.py
@@ -35,6 +35,10 @@ def bgs():
     MOG2.setShadowValue(255)
     MOG2.setShadowThreshold(0.4)
 
+
+    CNT = cv.bgsegm.createBackgroundSubtractorCNT()
+    KNN = cv.createBackgroundSubtractorKNN(detectShadows=True, dist2Threshold=1000)
+
     # Kernel für Morph. Operatoren
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))
 
@@ -47,6 +51,8 @@ def bgs():
 
         # BGS anwenden
         fgmask = MOG2.apply(frame)
+        fgmask2 = CNT.apply(frame)
+        fgmask3 = KNN.apply(frame)
 
         # Schatten von den Vordergrundmasken über Threshold entfernen
         #fgmask = cv.threshold(fgmask, 254, 255, cv.THRESH_BINARY)[1]
@@ -56,18 +62,34 @@ def bgs():
         # Median-Filter zur weiteren Rauschreduzierung
         fgmask = cv.medianBlur(fgmask, 3)
 
-        # Maske in BGR-Bild umwandeln, weil sonst die Darstellung von Farb- und Graubild von den Channeln her nicht passt
+        # ... Gleicher Code = Gleiche Doku s.o.
+        fgmask2 = cv.threshold(fgmask2, 254, 255, cv.THRESH_BINARY)[1]
+        fgmask2 = cv.morphologyEx(fgmask2, cv.MORPH_OPEN, kernel)
+        fgmask2 = cv.morphologyEx(fgmask2, cv.MORPH_CLOSE, kernel)
+        fgmask2 = cv.medianBlur(fgmask2, 3)
+
+        # ...
+        fgmask3 = cv.morphologyEx(fgmask3, cv.MORPH_OPEN, kernel)
+        fgmask3 = cv.morphologyEx(fgmask3, cv.MORPH_CLOSE, kernel)
+        fgmask3 = cv.medianBlur(fgmask3, 3)
+
+        # Masken in BGR-Bild umwandeln, weil sonst die Darstellung von Farb- und Graubild von den Channeln her nicht passt
         fgmask_bgr = cv.cvtColor(fgmask, cv.COLOR_GRAY2BGR)
+        fgmask2_bgr = cv.cvtColor(fgmask2, cv.COLOR_GRAY2BGR)
+        fgmask3_bgr = cv.cvtColor(fgmask3, cv.COLOR_GRAY2BGR)
 
         # Frames beschriften
         original_frame = frame.copy()
 
         labelFrame(original_frame, 'Original')
         labelFrame(fgmask_bgr, 'MOG2')
-
+        labelFrame(fgmask2_bgr, 'CNT')
+        labelFrame(fgmask3_bgr, 'KNN')
 
         # Frames zusammenfügen (Original, MOG, CNT, KNN)
-        combined_frame = np.hstack((original_frame, fgmask_bgr))
+        top_row = np.hstack((original_frame, fgmask_bgr))
+        bottom_row = np.hstack((fgmask3_bgr, fgmask2_bgr))
+        combined_frame = np.vstack((top_row, bottom_row))
 
         # Bild skalieren auf die oben angegebene Größe
         height, width = combined_frame.shape[:2]
@@ -77,7 +99,7 @@ def bgs():
         # Ergebnis der BGS
         cv.imshow('', resized_frame)
 
-        keyboard = cv.waitKey(25)
+        keyboard = cv.waitKey(1)
         if keyboard == ord('q') or keyboard == 27:  # 'q' oder Esc
             break
 
