f2c264d2ddd217c420a2de1a65e333be0db812d3
Lukas Landmann <lukaslandmann69@gmail.com>
2024-12-06 13:23:06 +0100

    Tracker überarbeitet

--- src/cv/tracker.py
+++ src/cv/tracker.py
@@ -25,9 +25,11 @@ class Detector:
         self.hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
 
     def detect(self, frame, fgmask):
-        frame_down_sample = cv.resize(frame, ((frame.shape[1] // 20) * 10, (frame.shape[0] // 20) * 10))
-        boxes, weights = self.hog.detectMultiScale(frame_down_sample, winStride=(8, 8), padding=(8, 8), scale=1.05)
-        boxes = np.divide(boxes * 20, 10).astype(int)  # Up-sampling der Koordinaten
+
+        frame_down_sample = cv.resize(frame, ((frame.shape[1] // 25) * 10, (frame.shape[0] // 25) * 10))
+        boxes, weights = self.hog.detectMultiScale(frame_down_sample, winStride=(8, 8), padding=(8, 8), scale=1.06)
+        boxes = np.divide(boxes * 25, 10).astype(int)  # Up-sampling der Koordinaten
+
         return self.filter_boxes(boxes, fgmask)
 
     @staticmethod
@@ -45,6 +47,7 @@ class Detector:
                     break
             else:
                 filtered_boxes.append(r)
+                print("FILTER")
         return [box for box in filtered_boxes if np.count_nonzero(fgmask[box[1]:box[1] + box[3], box[0]:box[0] + box[2]]) > min_area]
 
 
@@ -61,13 +64,15 @@ class Tracker:
 
         for (x, y, w, h) in boxes:
             roi = frame_gray[y:y + h, x:x + w]  # ROI um Feature-Suche einzugrenzen
+            #roi = frame_gray[(y+h)//2 - 250:(y+h)//2 + 250, (x+w)//2 - 150:(x+w)//2 + 150]  # ROI um Feature-Suche einzugrenzen
             features = cv.goodFeaturesToTrack(roi, **self.feature_params)
             if features is not None:
                 self.box_tracks.append(
                     {
                         "box": (x, y, w, h),
                         "features": [(px + x, py + y) for px, py in np.float32(features).reshape(-1, 2)],
-                        "mean_shift": [0, 0]
+                        "mean_shift": [0, 0],
+                        "center": [(x + w) // 2, (y + h) // 2]
                     }
                 )
 
@@ -77,10 +82,13 @@ class Tracker:
         for track in self.box_tracks:
             box = track["box"]
             features = np.float32(track["features"]).reshape(-1, 1, 2)
+            np.append(features, track["center"])
+
 
             # p1 = [    [[x1, y2]] ,    [[x2, y2]]      ]
             p1, st, err = cv.calcOpticalFlowPyrLK(prev_gray, frame_gray, features, None, **self.lk_params)
-            valid_points, previous_points = self._filter_valid_points(p1, st, features, fgmask)
+            p0, st0, err0 = cv.calcOpticalFlowPyrLK(frame_gray, prev_gray, p1, None, **self.lk_params)
+            valid_points, previous_points = self._filter_valid_points(p1, st, p0, track, fgmask)
 
             if len(valid_points) > 0:
                 # Berechne die durchschnittliche Verschiebung der Punkte
@@ -89,7 +97,6 @@ class Tracker:
                 mean_shift = np.mean(movement, axis=0)
 
                 x, y, w, h = box
-
                 dx = mean_shift[0]
                 dy = mean_shift[1]
 
@@ -101,18 +108,21 @@ class Tracker:
                         "box": (new_x, new_y, w, h),
                         "features": valid_points.tolist(),
                         "mean_shift": mean_shift,
+                        "center": [(x + w) // 2, (y + h) // 2]
                     })
 
         return points
 
 
     @staticmethod
-    def _filter_valid_points(p1, st, features, fgmask):
-
+    def _filter_valid_points(p1, st, features, box, fgmask):
+        #p1[box["center"][1] - 5:box["center"][1] + 5, box["center"][0] - 5:box["center"][0] + 5]
         valid_points = p1[st == 1].reshape(-1, 2)
         previous_points = features[st == 1].reshape(-1, 2)
         mask_filter = [
-            not np.all(fgmask[int(p[1]):int(p[1]) + 2, int(p[0]):int(p[0]) + 2] == 0)
+            not np.all(fgmask[int(p[1]):int(p[1]) + 5, int(p[0]):int(p[0]) + 5] == 0)
+            #and (int(p[1]) >= int(box["center"][1]) - 5) and (int(p[1]) <= int(box["center"][1]) + 5)
+            #and (int(p[0]) >= int(box["center"][0]) - 5) and (int(p[0]) <= int(box["center"][0]) + 5)
             for p in valid_points
         ]
         return valid_points[mask_filter], previous_points[mask_filter]
@@ -126,6 +136,7 @@ class Tracker:
             track["box"] = (x, y, w, h)
 
     def init_new_tracks(self):
+
         self.box_tracks = self.updated_box_tracks
         self.last_box_tracks = self.updated_box_tracks
         self.updated_box_tracks = []
@@ -138,8 +149,13 @@ class Tracker:
     def draw_features(self, vis, features):
         if features is not None:
             for feature_list in features:
-                for point in feature_list:
-                    cv.circle(vis, (int(point[0]), int(point[1])), 2, (0, 255, 0), -1)
+                for i, point in enumerate(feature_list):
+                    if i == len(feature_list) - 1:
+                        cv.circle(vis, (int(point[0]), int(point[1])), 2, (0, 0, 255), -1)
+                    else:
+                        cv.circle(vis, (int(point[0]), int(point[1])), 2, (0, 255, 0), -1)
+
+
 
 
 class SingleObjectTrackingPipeline:
@@ -161,17 +177,29 @@ class SingleObjectTrackingPipeline:
             frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
             fgmask = self.bgs.bgs_apply(frame)
 
+            print("yes", self.tracker.box_tracks)
             if len(self.tracker.box_tracks) < 1:
+                print("IM IN")
+                self.tracker.box_tracks.clear()
                 boxes = self.detector.detect(frame, fgmask)
+                print("ZEICHNE", len(boxes))
                 self.tracker.init_tracks(boxes, frame_gray)
+                self.tracker.draw_boxes(vis, self.tracker.box_tracks)
+
             else:
                 points = self.tracker.update_tracks(self.prev_gray, frame_gray, fgmask)
                 self.tracker.draw_features(vis, points)
 
-            if len(self.tracker.updated_box_tracks) == 0:
+            if len(self.tracker.updated_box_tracks) == 0 and len(self.tracker.box_tracks) == 0:
                 if len(self.tracker.last_box_tracks) > 0:
                     self.tracker.virtual_movement()
+                    self.tracker.box_tracks.clear()
                     self.tracker.draw_boxes(vis, self.tracker.last_box_tracks)
+            elif len(self.tracker.updated_box_tracks) == 0 and len(self.tracker.box_tracks) >=1:
+                points = self.tracker.update_tracks(self.prev_gray, frame_gray, fgmask)
+                self.tracker.draw_features(vis, points)
+                self.tracker.init_new_tracks()
+                self.tracker.draw_boxes(vis, self.tracker.box_tracks)
             else:
                 self.tracker.init_new_tracks()
                 self.tracker.draw_boxes(vis, self.tracker.box_tracks)
